2a1a8ca996567d627fa5535e2df8892b09dcaca1
Bilal Baheer <71363925+BilalBaheer@users.noreply.github.com>
2025-03-12 11:31:32 -0700

    Update SlidingWindow.cpp

--- SlidingWindow.cpp
+++ SlidingWindow.cpp
@@ -17,3 +17,23 @@ public:
     }
 };
  
+/*
+// another possible solution
+int lengthOfLongestSubstring(string s) {
+    int i = 0;  // Left pointer for the sliding window
+    int ans = 0; // Variable to store the maximum length of the substring
+    int cnt[128] = {}; // Array to track character occurrences (ASCII size 128)
+    for (int j = 0; j < s.size(); j++) {
+        cnt[s[j]]++; // Increment count of the current character
+        // If a character appears more than once, move the left pointer (i)
+        while (cnt[s[j]] > 1) {
+            cnt[s[i]]--; // Decrease count of the leftmost character
+            i++; // Move the left pointer forward
+        }
+        // Update the maximum length found so far
+        ans = max(ans, j - i + 1);
+    }
+    return ans;
+}
+
+*/
